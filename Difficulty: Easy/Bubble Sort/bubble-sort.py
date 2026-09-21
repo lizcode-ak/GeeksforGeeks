0c850704549775bb6df882d9d04fc71d79921272
Arkendu Chatterjee <66506216+lizcode-ak@users.noreply.github.com>
2026-09-21 22:39:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Bubble Sort/bubble-sort.py
@@ -0,0 +1,14 @@
+class Solution:
+    def bubbleSort(self,arr):
+        # code here
+        n = len(arr)
+        for i in range(n):
+            swapped = False
+            for j in range(0,n-1-i):
+                if arr[j] > arr[j+1]:
+                    arr[j], arr[j+1] = arr[j+1], arr[j]
+                    swapped = True
+            
+            if not swapped:
+                break
+        return arr
