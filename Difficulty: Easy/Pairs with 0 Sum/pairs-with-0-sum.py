6493c085c7d1231a8ffc7beeb86269645fa3ed95
Arkendu Chatterjee <66506216+lizcode-ak@users.noreply.github.com>
2026-09-22 14:18:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pairs with 0 Sum/pairs-with-0-sum.py
@@ -0,0 +1,28 @@
+class Solution:
+    def getPairs(self, arr):
+        elements = set(arr)
+        res = []
+        for x in elements:
+            if x < 0 and -x in elements:
+                res.append([x, -x])
+        if 0 in elements and arr.count(0) >= 2:
+            res.append([0, 0])
+
+        res.sort()
+        return res
+        
+        
+        
+        
+        
+        
+        
+        
+        '''n = len(arr)
+        res = []
+        for i in range(n):
+            for j in range(i+1,n):
+                if arr[i] + arr[j] == 0:
+                    res.append([i,j])
+        
+        return res'''
